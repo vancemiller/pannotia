9c1d9b3b326ce4b56d32af0d9418e2cf6adae114
Vance Miller <vance@cs.utexas.edu>
2017-04-18 10:56:00 -0500

    timing information

--- cuda/nn/hurricane_gen.c
+++ cuda/nn/hurricane_gen.c
@@ -2,23 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 // 641986 gets you ~30 MB of data
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   FILE *fp;
-  int i=0, canes=0,num_files=0,j=0;
-  int year,month,date,hour,num,speed,press;
-  float lat,lon;
-  int hours[4] = {0,6,12,18};
+  int i = 0, canes = 0, num_files = 0, j = 0;
+  int year, month, date, hour, num, speed, press;
+  float lat, lon;
+  int hours[4] = { 0, 6, 12, 18 };
   char *name, fname[16];
-  char names[21][10] = {"ALBERTO", "BERYL", "CHRIS","DEBBY","ERNESTO","FLORENCE","GORDON",
-      "HELENE","ISAAC","JOYCE","KIRK","LESLIE","MICHAEL","NADINE","OSCAR","PATTY","RAFAEL",
-      "SANDY","TONY","VALERIE","WILLIAM"};
+  char names[21][10] = { "ALBERTO", "BERYL", "CHRIS", "DEBBY", "ERNESTO", "FLORENCE", "GORDON",
+      "HELENE", "ISAAC", "JOYCE", "KIRK", "LESLIE", "MICHAEL", "NADINE", "OSCAR", "PATTY", "RAFAEL",
+      "SANDY", "TONY", "VALERIE", "WILLIAM" };
 
-  if(argc < 3)
-  {
-    fprintf(stderr,"we need a number of hurricanes to create and number of files to create\n");
+  if (argc < 3) {
+    fprintf(stderr, "we need a number of hurricanes to create and number of files to create\n");
     exit(0);
   }
 
@@ -26,19 +23,18 @@ int main(int argc, char **argv)
   num_files = atoi(argv[2]);
 
   canes = (canes / num_files) + 1;
-  
-  for(j=0;j<num_files;j++)
-  {
-    sprintf(fname, "data/cane%d_%d.db", num_files,j);
-    
-    if ((fp = fopen(fname, "w")) == NULL) {
-		 fprintf(stderr, "Failed to open output file '%s'!\n", fname);
-		 return -1;
-	 }
-  
-    srand(time(NULL));
 
-    for(i=0;i<canes;i++)
+  for (j = 0; j < num_files; j++) {
+    sprintf(fname, "data/cane%d_%d.db", num_files, j);
+
+    if ((fp = fopen(fname, "w")) == NULL) {
+      fprintf(stderr, "Failed to open output file '%s'!\n", fname);
+      return -1;
+    }
+
+    srand (time(NULL));
+
+for(    i=0;i<canes;i++)
     {
       year = 1950 + rand() % 55;
       month = 1 + rand() % 12;
@@ -47,17 +43,17 @@ int main(int argc, char **argv)
       num = 1 + rand() % 28;
       name = names[rand()%21];
       lat = ((float)(7 + rand() % 63)) + ((float) rand() / (float) 0x7fffffff);
-      lon = ((float)(rand() % 358)) + ((float) rand() / (float) 0x7fffffff); 
+      lon = ((float)(rand() % 358)) + ((float) rand() / (float) 0x7fffffff);
       speed = 10+ rand() % 155;
       press = rand() % 900;
 
-      fprintf(fp, "%4d %2d %2d %2d %2d %-9s %5.1f %5.1f %4d %4d\n", year, month, date, hour, num, name, lat, lon, speed, press); 
+      fprintf(fp, "%4d %2d %2d %2d %2d %-9s %5.1f %5.1f %4d %4d\n", year, month, date, hour, num, name, lat, lon, speed, press);
 
     }
 
     fclose(fp);
   }
 
-    return 0;
+  return 0;
 
-  }
+}
--- cuda/nn/nn_cuda.cu
+++ cuda/nn/nn_cuda.cu
@@ -25,44 +25,42 @@
 #define OPEN 10000  // initial value of nearest neighbors
 
 #define TIMESTAMP(NAME) \
-struct timespec NAME; \
+  struct timespec NAME; \
 if (clock_gettime(CLOCK_MONOTONIC, &NAME)) { \
   fprintf(stderr, "Failed to get time: %s\n", strerror(errno)); \
 }
 
 #define ELAPSED(start, end) \
-(long long int) 1e9 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec
+  ((long long int) 1e9 * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec)
 
-typedef struct latLong
-{
+typedef struct latLong {
   float lat;
   float lng;
 } LatLong;
 
-typedef struct record
-{
+typedef struct record {
   char recString[REC_LENGTH];
   float distance;
 } Record;
 
 int loadData(char *filename, std::vector<Record> &records, LatLong** locations, bool unified);
-void findLowest(std::vector<Record> &records,float *distances,int numRecords,int topN);
+void findLowest(std::vector<Record> &records, float *distances, int numRecords, int topN);
 void printUsage();
-int parseCommandline(int argc, char *argv[], char* filename,int *r,float *lat,float *lng,
-    int *q, int *t, int *p, int *d, bool* u);
+int parseCommandline(int argc, char *argv[], char* filename, int *r, float *lat, float *lng, int *q,
+    int *t, int *p, int *d, bool* u);
 
 /**
  * Kernel
  * Executed on GPU
  * Calculates the Euclidean distance from each record in the database to the target position
  */
-__global__ void euclid(LatLong* locations, float* distances, int numRecords,float lat, float lng) {
-  int globalId = blockDim.x * ( gridDim.x * blockIdx.y + blockIdx.x ) + threadIdx.x;
+__global__ void euclid(LatLong* locations, float* distances, int numRecords, float lat, float lng) {
+  int globalId = blockDim.x * (gridDim.x * blockIdx.y + blockIdx.x) + threadIdx.x;
   LatLong* latLong = locations + globalId;
   if (globalId < numRecords) {
     float* dist = distances + globalId;
-    *dist = (float) sqrt((lat - latLong->lat) * (lat - latLong->lat) + (lng - latLong->lng) *
-        (lng - latLong->lng));
+    *dist = (float) sqrt(
+        (lat - latLong->lat) * (lat - latLong->lat) + (lng - latLong->lng) * (lng - latLong->lng));
   }
 }
 
@@ -70,7 +68,13 @@ __global__ void euclid(LatLong* locations, float* distances, int numRecords,floa
  * This program finds the k-nearest neighbors
  **/
 int main(int argc, char* argv[]) {
-  int    i = 0;
+  long long time_serial = 0;
+  long long time_copy_in = 0;
+  long long time_copy_out = 0;
+  long long time_kernel = 0;
+  long long time_malloc = 0;
+  long long time_free = 0;
+  int i = 0;
   float lat, lng;
   int quiet = 0, timing = 0, platform = 0, device = 0;
   bool unified = false;
@@ -78,17 +82,18 @@ int main(int argc, char* argv[]) {
   std::vector<Record> records;
   LatLong* locations;
   char filename[100];
-  int resultsCount=10;
+  int resultsCount = 10;
 
   // parse command line
-  if (parseCommandline(argc, argv, filename, &resultsCount, &lat, &lng,
-        &quiet, &timing, &platform, &device, &unified)) {
+  if (parseCommandline(argc, argv, filename, &resultsCount, &lat, &lng, &quiet, &timing, &platform,
+        &device, &unified)) {
     printUsage();
     return 0;
   }
 
   int numRecords = loadData(filename, records, &locations, unified);
-  if (resultsCount > numRecords) resultsCount = numRecords;
+  if (resultsCount > numRecords)
+    resultsCount = numRecords;
 
   //Pointers to host memory
   float *distances;
@@ -108,45 +113,46 @@ int main(int argc, char* argv[]) {
   cudaDeviceSynchronize();
   unsigned long usableDeviceMemory = freeDeviceMemory * 85 / 100; // 85% arbitrary throttle to compensate for known CUDA bug
   unsigned long maxThreads = usableDeviceMemory / 12; // 4 bytes in 3 vectors per thread
-  if ( numRecords > maxThreads )
-  {
-    fprintf( stderr, "Error: Input too large.\n" );
-    exit( 1 );
+  if (numRecords > maxThreads) {
+    fprintf(stderr, "Error: Input too large.\n");
+    exit(1);
   }
-  unsigned long blocks = ceilDiv( numRecords, threadsPerBlock ); // extra threads will do nothing
-  unsigned long gridY = ceilDiv( blocks, maxGridX );
-  unsigned long gridX = ceilDiv( blocks, gridY );
+  unsigned long blocks = ceilDiv(numRecords, threadsPerBlock); // extra threads will do nothing
+  unsigned long gridY = ceilDiv(blocks, maxGridX);
+  unsigned long gridX = ceilDiv(blocks, gridY);
   // There will be no more than (gridY - 1) extra blocks
-  dim3 gridDim( gridX, gridY );
+  dim3 gridDim(gridX, gridY);
 
-  if ( DEBUG )
-  {
-    print( totalDeviceMemory ); // 804454400
-    print( freeDeviceMemory );
-    print( usableDeviceMemory );
-    print( maxGridX ); // 65535
-    print( deviceProp.maxThreadsPerBlock ); // 1024
-    print( threadsPerBlock );
-    print( maxThreads );
-    print( blocks ); // 130933
-    print( gridY );
-    print( gridX );
+  if (DEBUG) {
+    print(totalDeviceMemory); // 804454400
+    print(freeDeviceMemory);
+    print(usableDeviceMemory);
+    print(maxGridX); // 65535
+    print(deviceProp.maxThreadsPerBlock); // 1024
+    print(threadsPerBlock);
+    print(maxThreads);
+    print(blocks); // 130933
+    print(gridY);
+    print(gridX);
   }
 
   cudaStream_t stream;
   assert(cudaStreamCreate(&stream) == cudaSuccess);
 
-  TIMESTAMP(ts1);
+  TIMESTAMP(t0);
   /**
    * Allocate memory on host and device
    */
   if (!unified) {
     distances = (float *) malloc(sizeof(float) * numRecords);
-    cudaMalloc((void **) &d_locations,sizeof(LatLong) * numRecords);
-    cudaMalloc((void **) &d_distances,sizeof(float) * numRecords);
+    cudaMalloc((void **) &d_locations, sizeof(LatLong) * numRecords);
+    cudaMalloc((void **) &d_distances, sizeof(float) * numRecords);
   } else {
     assert(cudaMallocManaged(&distances, sizeof(float) * numRecords) == cudaSuccess);
   }
+
+  TIMESTAMP(t1);
+  time_malloc += ELAPSED(t0, t1);
 
   /**
    * Transfer data from host to device
@@ -154,10 +160,10 @@ int main(int argc, char* argv[]) {
   if (!unified) {
     cudaMemcpyAsync(d_locations, locations, sizeof(LatLong) * numRecords, cudaMemcpyHostToDevice,
         stream);
+    cudaStreamSynchronize(stream);
   }
-  cudaStreamSynchronize(stream);
-
-  TIMESTAMP(ts2);
+  TIMESTAMP(t2);
+  time_copy_in += ELAPSED(t1, t2);
 
   /**
    * Execute kernel
@@ -168,30 +174,31 @@ int main(int argc, char* argv[]) {
     euclid<<<gridDim, threadsPerBlock, 0, stream>>>(locations, distances, numRecords, lat, lng);
   }
   cudaStreamSynchronize(stream);
-
-  TIMESTAMP(ts3);
+  TIMESTAMP(t3);
+  time_kernel += ELAPSED(t2, t3);
 
   /**
    * Copy data from device memory to host memory
    */
   if (!unified) {
-    cudaMemcpyAsync(distances, d_distances, sizeof(float)*numRecords, cudaMemcpyDeviceToHost, stream);
+    cudaMemcpyAsync(distances, d_distances, sizeof(float) * numRecords, cudaMemcpyDeviceToHost, stream);
     cudaStreamSynchronize(stream);
   }
-
-  TIMESTAMP(ts4);
+  TIMESTAMP(t4);
+  time_copy_out += ELAPSED(t3, t4);
 
   // find the resultsCount least distances
   findLowest(records, distances, numRecords, resultsCount);
 
-  TIMESTAMP(ts5);
-
   // print out results
   if (!quiet) {
-    for(i=0;i<resultsCount;i++) {
-      printf("%s --> Distance=%f\n",records[i].recString,records[i].distance);
+    for (i = 0; i < resultsCount; i++) {
+      printf("%s --> Distance=%f\n", records[i].recString, records[i].distance);
     }
   }
+  TIMESTAMP(t5);
+  time_serial += ELAPSED(t4, t5);
+
   if (!unified) {
     free(locations);
     free(distances);
@@ -202,23 +209,24 @@ int main(int argc, char* argv[]) {
     cudaFree(locations);
     cudaFree(distances);
   }
+  TIMESTAMP(t6);
+  time_free += ELAPSED(t5, t6);
 
-  TIMESTAMP(ts6);
-
-  printf("Malloc + Copy in: %9lld ns\n", ELAPSED(ts1, ts2));
-  printf("Kernel execution: %9lld ns\n", ELAPSED(ts2, ts3));
-  printf("Copy out:         %9lld ns\n", ELAPSED(ts3, ts4));
-  printf("CPU Processing:   %9lld ns\n", ELAPSED(ts4, ts5));
-  printf("Free:             %9lld ns\n", ELAPSED(ts5, ts6));
-  printf("TOTAL:            %9lld ns\n", ELAPSED(ts1, ts6));
-
+  printf("====Timing info====\n");
+  printf("time serial = %f ms\n", time_serial * 1e-6);
+  printf("time GPU malloc = %f ms\n", time_malloc * 1e-6);
+  printf("time CPU to GPU memory copy = %f ms\n", time_copy_in * 1e-6);
+  printf("time kernel = %f ms\n", time_kernel * 1e-6);
+  printf("time GPU to CPU memory copy back = %f ms\n", time_copy_out * 1e-6);
+  printf("time GPU free = %f ms\n", time_free * 1e-6);
+  printf("End-to-end = %f ms\n", ELAPSED(t0, t6) * 1e-6);
 }
 
-int loadData (char *filename, std::vector<Record> &records, LatLong** loc, bool unified) {
-  FILE   *flist, *fp;
-  int    i = 0;
+int loadData(char *filename, std::vector<Record> &records, LatLong** loc, bool unified) {
+  FILE *flist, *fp;
+  int i = 0;
   char dbname[64];
-  int recNum=0;
+  int recNum = 0;
   std::vector<LatLong> locations;
 
   /**Main processing **/
@@ -243,18 +251,21 @@ int loadData (char *filename, std::vector<Record> &records, LatLong** loc, bool 
     while (!feof(fp)) {
       Record record;
       LatLong latLong;
-      fgets(record.recString,49,fp);
+      fgets(record.recString, 49, fp);
       fgetc(fp); // newline
-      if (feof(fp)) break;
+      if (feof(fp))
+        break;
 
       // parse for lat and long
       char substr[6];
 
-      for(i=0;i<5;i++) substr[i] = *(record.recString+i+28);
+      for (i = 0; i < 5; i++)
+        substr[i] = *(record.recString + i + 28);
       substr[5] = '\0';
       latLong.lat = atof(substr);
 
-      for(i=0;i<5;i++) substr[i] = *(record.recString+i+33);
+      for (i = 0; i < 5; i++)
+        substr[i] = *(record.recString + i + 33);
       substr[5] = '\0';
       latLong.lng = atof(substr);
 
@@ -278,18 +289,19 @@ int loadData (char *filename, std::vector<Record> &records, LatLong** loc, bool 
   return recNum;
 }
 
-void findLowest(std::vector<Record> &records,float *distances,int numRecords,int topN){
-  int i,j;
+void findLowest(std::vector<Record> &records, float *distances, int numRecords, int topN) {
+  int i, j;
   float val;
   int minLoc;
   Record *tempRec;
   float tempDist;
 
-  for(i=0;i<topN;i++) {
+  for (i = 0; i < topN; i++) {
     minLoc = i;
-    for(j=i;j<numRecords;j++) {
+    for (j = i; j < numRecords; j++) {
       val = distances[j];
-      if (val < distances[minLoc]) minLoc = j;
+      if (val < distances[minLoc])
+        minLoc = j;
     }
     // swap locations and distances
     tempRec = &records[i];
@@ -305,15 +317,16 @@ void findLowest(std::vector<Record> &records,float *distances,int numRecords,int
   }
 }
 
-int parseCommandline(int argc, char *argv[], char* filename,int *r,float *lat,float *lng,
-    int *q, int *t, int *p, int *d, bool* u){
+int parseCommandline(int argc, char *argv[], char* filename, int *r, float *lat, float *lng, int *q,
+    int *t, int *p, int *d, bool* u) {
   int i;
-  if (argc < 2) return 1; // error
-  strncpy(filename,argv[1],100);
+  if (argc < 2)
+    return 1; // error
+  strncpy(filename, argv[1], 100);
   char flag;
 
-  for(i=1;i<argc;i++) {
-    if (argv[i][0]=='-') {// flag
+  for (i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') { // flag
       flag = argv[i][1];
       switch (flag) {
         case 'r': // number of results
@@ -321,11 +334,10 @@ int parseCommandline(int argc, char *argv[], char* filename,int *r,float *lat,fl
           *r = atoi(argv[i]);
           break;
         case 'l': // lat or lng
-          if (argv[i][2]=='a') {//lat
-            *lat = atof(argv[i+1]);
-          }
-          else {//lng
-            *lng = atof(argv[i+1]);
+          if (argv[i][2] == 'a') { //lat
+            *lat = atof(argv[i + 1]);
+          } else { //lng
+            *lng = atof(argv[i + 1]);
           }
           i++;
           break;
@@ -351,15 +363,16 @@ int parseCommandline(int argc, char *argv[], char* filename,int *r,float *lat,fl
       }
     }
   }
-  if ((*d >= 0 && *p<0) || (*p>=0 && *d<0)) // both p and d must be specified if either are specified
+  if ((*d >= 0 && *p < 0) || (*p >= 0 && *d < 0)) // both p and d must be specified if either are specified
     return 1;
   return 0;
 }
 
-void printUsage(){
+void printUsage() {
   printf("Nearest Neighbor Usage\n");
   printf("\n");
-  printf("nearestNeighbor [filename] -r [int] -lat [float] -lng [float] [-hqt] [-p [int] -d [int]]\n");
+  printf(
+      "nearestNeighbor [filename] -r [int] -lat [float] -lng [float] [-hqt] [-p [int] -d [int]]\n");
   printf("\n");
   printf("example:\n");
   printf("$ ./nearestNeighbor filelist.txt -r 5 -lat 30 -lng 90\n");
